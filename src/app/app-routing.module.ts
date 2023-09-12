b7d025996a80f16c85639624c8c281d64ee34932
Erlin Sangay <24_de_7@protonmail.com>
2023-09-12 12:50:21 -0500

    Separando login/register del template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { RegisterComponent } from './auth/register/register.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
   { path: '', component: PagesComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'grafica1', component: Grafica1Component, },
-
+      { path: 'progress', component: ProgressComponent },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full', },
     ]
   },
