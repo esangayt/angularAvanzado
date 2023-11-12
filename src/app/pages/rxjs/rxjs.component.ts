303e42990e8114b75a3ec1aa80443e13ac5141b3
sS_Tr33s <24_de_7@protonmail.com>
2023-11-11 22:51:25 -0500

    Subscriber básico

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {logMessages} from "@angular-devkit/build-angular/src/tools/esbuild/utils";
 
 @Component({
   selector: 'app-rxjs',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./rxjs.component.css']
 })
 export class RxjsComponent {
+  constructor() {
+    this.basicRXJS()
+  }
 
+  basicRXJS(){
+   const observable = new Observable(observer=>{
+     let i = 0
+
+     const inter =
+       setInterval(()=>{
+       i++
+       observer.next(i)
+
+       if (i === 6){
+         clearInterval(inter)
+         observer.complete()
+       }
+
+       if (i == 2){
+         observer.error("ya estás en 2")
+       }
+
+     }, 1000)
+   })
+
+   observable.subscribe(
+     (value) => console.log(value),
+     (error) => console.warn(error),
+     () => console.log("al finalizar en subscriber"),
+   )
+  }
 }
