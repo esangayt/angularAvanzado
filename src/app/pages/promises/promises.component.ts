e1610b361810001de91b6b7ce426ff1cd4765abf
sS_Tr33s <24_de_7@protonmail.com>
2023-11-11 22:15:45 -0500

    Ejemplos de Promesas
    Básico
    Petición HTTP

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./promises.component.css']
 })
 export class PromisesComponent {
+  constructor() {
+    this.basicPromise().then((resolve)=>{
+      console.log(resolve)
+    }).catch((reject)=>{
+      console.log(reject)
+    })
 
+    this.intermediatePromise().then((resolve)=>{
+      console.log(resolve)
+    })
+  }
+
+  basicPromise() {
+    return new Promise((resolve, reject)=>{
+      if(false){
+        resolve('Hey! It worked')
+      }else{
+        reject('Something went wrong')
+      }
+    })
+  }
+
+  intermediatePromise(){
+    // const api = fetch('https://reqres.in/api/users?page=2')
+    //   .then((response)=>{
+    //     console.log(response)
+    //     return response.json()
+    //   })
+    //   .then((user)=>{
+    //     console.log(user.data)
+    //   })
+
+    return new Promise((resolve, reject)=> {
+      fetch('https://reqres.in/api/users?page=2')
+        .then((response) => response.json())
+        .then((user) => resolve(user.data))
+    })
+  }
 }
